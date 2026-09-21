e258d735a1016cb8443dbf24a1b52539baa118e6
Joshua Luke Parris <130725599+joshualparris@users.noreply.github.com>
2026-09-21 15:51:45 +1000

    Support GitHub Pages base path

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ async function start() {
   await initialiseDatabase();
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '') || '/'}>
         <App />
       </BrowserRouter>
     </React.StrictMode>
